23d09a8307428105c663f9f956f108190b51e555
Renata <renatacristovams@gmail.com>
2021-05-21 15:38:08 -0300

    Using view binding to Set layout

--- app/src/main/java/com/example/favdash/SplashActivity.kt
+++ app/src/main/java/com/example/favdash/SplashActivity.kt
@@ -2,10 +2,17 @@ package com.example.favdash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.favdash.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        //Isso:
+        //setContentView(R.layout.activity_splash)
+        //Se torna isso:
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+       // splashBinding.tvAppName.text = "Hello world"
     }
 }
