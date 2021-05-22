33ddfabfaee696868eab87e583efc6147ee5971f
Renata <renatacristovams@gmail.com>
2021-05-22 00:15:10 -0300

    Creating Animations

--- app/src/main/java/com/example/favdash/SplashActivity.kt
+++ app/src/main/java/com/example/favdash/SplashActivity.kt
@@ -1,7 +1,15 @@
 package com.example.favdash
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.favdash.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -14,5 +22,40 @@ class SplashActivity : AppCompatActivity() {
         setContentView(splashBinding.root)
 
        // splashBinding.tvAppName.text = "Hello world"
+
+        //For the screen to fill all screen above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        //For active my animation (I created at R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+        //Animation listener for animation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+              //
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                //Realize this after 1 second after the of animation
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
