0951dc6d4736bac9bd0e97a1fb1cf27b7cfc630c
Renata <renatacristovams@gmail.com>
2021-05-22 01:01:50 -0300

    Structuring packages for MVVM architecture

--- app/src/main/java/com/example/favdash/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/favdash/view/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.favdash
+package com.example.favdash.view.activities
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.favdash.R
 import com.example.favdash.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
