75d9982f4f901ba587ba60f6bbf9626b26eb9c78
Renata <renatacristovams@gmail.com>
2021-05-23 18:26:54 -0300

    Taking Dialog xml using view bind and Implementing taken image of the camera

--- app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
@@ -10,15 +10,22 @@ import com.example.favdash.databinding.ActivityAddUpdateDishBinding
 import com.example.favdash.databinding.DialogCustomImageSelectionBinding
 import com.karumi.dexter.Dexter
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
+import android.provider.MediaStore
 import android.provider.Settings
+import androidx.core.content.ContextCompat
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -42,8 +49,8 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        if(v != null) {
-            when(v.id) {
+        if (v != null) {
+            when (v.id) {
                 R.id.iv_add_dish_image -> {
                     //Toast.makeText(this, "You have clicked the imageView", Toast.LENGTH_LONG).show()
                     customImageSelectionDialog()
@@ -55,45 +62,21 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun customImageSelectionDialog() {
         val dialog = Dialog(this)
-        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener {
-            //Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
-            //dialog.dismiss()
-            Dexter.withContext(this).withPermissions(
+           Dexter.withContext(this).withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA
-            ).withListener(object: MultiplePermissionsListener {
+            ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if(report!!.areAllPermissionsGranted()) {
-                        Toast.makeText(this@AddUpdateDishActivity, "You have Camera permission now", Toast.LENGTH_LONG).show()
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
-            }).onSameThread().check()
-            //If you want to receive permission listener callbacks on the same thread that fired the permission request, you just need to call
-        // onSameThread before checking for permissions
-        }
-
-        binding.tvGallery.setOnClickListener {
-            //Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
-            //dialog.dismiss()
-            Dexter.withContext(this).withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            ).withListener(object: MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if(report!!.areAllPermissionsGranted()) {
-                        Toast.makeText(this@AddUpdateDishActivity, "You have Gallery permission now", Toast.LENGTH_LONG).show()
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
                     }
                 }
 
@@ -108,7 +91,49 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             // onSameThread before checking for permissions
         }
 
+        binding.tvGallery.setOnClickListener {
+           Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ).withListener(object : PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDishActivity,
+                        "You have Gallery permission now", Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDishActivity,
+                        "You have denied storage permission to select image", Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread().check()
+            //If you want to receive permission listener callbacks on the same thread that fired the permission request, you just need to call
+            // onSameThread before checking for permissions
+        }
         dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK) {
+            if(requestCode == CAMERA) {
+                data?.let {
+                    //The image taken is defined in the ImageView
+                    val thumbnails: Bitmap = data.extras!!.get("data") as Bitmap
+                    mBinding.ivDishImage.setImageBitmap(thumbnails)
+                    mBinding.ivAddDishImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_vector_edit))
+                }
+            }
+        }
     }
 
     private fun showRationalDialogForPermissions() {
@@ -131,5 +156,11 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
+
     }
+
+    companion object {
+        private const val CAMERA = 1
+    }
+
 }
