4f41b84cb8db0c1e8c9d365232e04225d6247455
Renata <renatacristovams@gmail.com>
2021-05-23 12:51:48 -0300

    Creating the addUpdateDish screen and using a library intuit.sdp

--- app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,40 @@ package com.example.favdash.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.favdash.R
+import com.example.favdash.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_add_update_dish)
+        setContentView(mBinding.root)
+        setupActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null) {
+            when(v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked the imageView", Toast.LENGTH_LONG).show()
+                    return
+                }
+            }
+        }
     }
 }
