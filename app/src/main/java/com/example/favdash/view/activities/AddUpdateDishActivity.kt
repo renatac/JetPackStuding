195e03eb45b8ae0ce2193f569de03b79f53664e4
Renata <renatacristovams@gmail.com>
2021-05-23 15:25:07 -0300

    Creating a Custom Dialog for user choose between camera and gallery

--- app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.example.favdash.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.favdash.R
 import com.example.favdash.databinding.ActivityAddUpdateDishBinding
+import com.example.favdash.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -32,10 +34,29 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if(v != null) {
             when(v.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this, "You have clicked the imageView", Toast.LENGTH_LONG).show()
+                    //Toast.makeText(this, "You have clicked the imageView", Toast.LENGTH_LONG).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
