8c4e1f54b59275069fff1e4ac1559fd9813d538c
Renata <renatacristovams@gmail.com>
2021-05-23 17:09:54 -0300

    Using the library Karumi Dexter tosimplify the process of requesting permissions at runtime

--- app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favdash/view/activities/AddUpdateDishActivity.kt
@@ -8,6 +8,17 @@ import android.widget.Toast
 import com.example.favdash.R
 import com.example.favdash.databinding.ActivityAddUpdateDishBinding
 import com.example.favdash.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -48,15 +59,77 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
-            dialog.dismiss()
+            //Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            //dialog.dismiss()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddUpdateDishActivity, "You have Camera permission now", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread().check()
+            //If you want to receive permission listener callbacks on the same thread that fired the permission request, you just need to call
+        // onSameThread before checking for permissions
         }
 
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
-            dialog.dismiss()
+            //Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            //dialog.dismiss()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()) {
+                        Toast.makeText(this@AddUpdateDishActivity, "You have Gallery permission now", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread().check()
+            //If you want to receive permission listener callbacks on the same thread that fired the permission request, you just need to call
+            // onSameThread before checking for permissions
         }
 
         dialog.show()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage(
+            "It looks like you have turned off permissions required for this feature. " +
+                    "It can be enabled under Application Settings"
+        )
+            .setPositiveButton("GO TO SETTING")
+            { _, _ ->
+                try {
+                    //Move the user to favDissh app settings
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
